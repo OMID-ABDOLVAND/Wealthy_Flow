c6201bfd7b65f3040f634d4d1bc851f9bd90865f
OMID-ABDOLVAND <omidabdolvand.be@gmail.com>
2024-06-01 10:14:20 -0700

    feat: add init ModelViewSet

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from djongo.models import Q
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from transactions.models import Category, Transaction
+from transactions.serializaers import CategorySerializer, TransactionSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Category.objects.filter(Q(user=user) | Q(default=True))
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Transaction.objects.filter(transactions_bank=user)
