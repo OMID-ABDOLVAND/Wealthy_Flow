4bd5d35a84afa15dcd427f643cbd92befbfd503f
OMID-ABDOLVAND <omidabdolvand.be@gmail.com>
2024-06-05 14:23:42 -0700

    feat: add status to serializer

--- transactions/serializaers.py
+++ transactions/serializaers.py
@@ -15,5 +15,5 @@ class TransactionSerializer(ModelSerializer):
     # TODO:handle bank account and category joins
     class Meta:
         model = Transaction
-        fields = ['transaction_id', 'transaction_type', 'amount', 'date', 'description', ]
-        read_only_fields = ['transaction_id', 'date']
+        fields = ['transaction_id', 'transaction_type', 'transaction_status', 'amount', 'date', 'description', ]
+        read_only_fields = ['transaction_id', 'date', 'transaction_status']
