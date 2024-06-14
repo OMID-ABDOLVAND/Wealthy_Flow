d79efa2d0fea4298e016798ecf83255a487a9b45
OMID-ABDOLVAND <omidabdolvand.be@gmail.com>
2024-06-14 03:37:15 -0700

    feat: add BankAccountViewSet

--- bank_accounts/views.py
+++ bank_accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+
+from bank_accounts.models import BankAccount
+from bank_accounts.serializers import BankAccountSerializer
+from config.permissions import IsOwnerOrAdmin
+
+
+class BankAccountViewSet(ModelViewSet):
+    serializer_class = BankAccountSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrAdmin]
+
+    def get_queryset(self):
+        return BankAccount.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
